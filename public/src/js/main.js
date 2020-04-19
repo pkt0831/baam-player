feb67b9202f3e476af1df959572b32984a64b2ec
Seongkyun-Yu <ysungkyun@gmail.com>
2020-04-19 14:58:19 +0900

    feat: write  test code

---
+++ public/src/js/main.js
@@ -0,0 +1,4 @@
+const $musicPlayer = document.querySelector('musicPlayer');
+const $playBtn = document.querySelector('player-play');
+const $prevBtn = document.querySelector('player-prev');
+const $nextBtn = document.querySelector('player-next');
