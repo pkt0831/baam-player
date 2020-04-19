535cb161cba6feb55cb4fc7c169f3d6b192bbc1c
Seongkyun-Yu <ysungkyun@gmail.com>
2020-04-19 19:56:35 +0900

    fix: delete Example filse

--- public/src/js/main.js
+++
@@ -1,4 +0,0 @@
-const $musicPlayer = document.querySelector('musicPlayer');
-const $playBtn = document.querySelector('player-play');
-const $prevBtn = document.querySelector('player-prev');
-const $nextBtn = document.querySelector('player-next');
