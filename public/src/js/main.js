73a9f9d7ebea6b8265ea682cb63e7100b0d13f6a
Seongkyun-Yu <ysungkyun@gmail.com>
2020-04-20 15:22:10 +0900

    feat: Make shuffle, selectedPlay, paintSelectedList funcs..

--- public/src/js/main.js
+++ public/src/js/main.js
@@ -4,6 +4,10 @@ const $playBtn = document.querySelector('.player-play');
 const $prevBtn = document.querySelector('.player-prev');
 const $nextBtn = document.querySelector('.player-next');
 const $musicPlayer = document.querySelector('.musicPlayer');
+const $progOuter = document.querySelector('.play-bar-outer');
+const $progGetevent = document.querySelector('.play-bar-getevent');
+const $shuffleBtn = document.querySelector('.player-shuffle');
+const $playList = document.querySelector('.play-list');
 
 
 let nowPlayList = [];
@@ -14,27 +18,52 @@ window.onload = async () => {
 
   const { data } = await axios.post('/playlist', { id });
   nowPlayList = data;
+
+  player.setPlaylist(nowPlayList);
   player.listRender(nowPlayList);
 };
 
-$playBtn.onclick = () => {
+$playBtn.addEventListener('click', () => {
   if (nowPlayList.length === 0) return;
 
-  player.setMusic(nowPlayList);
+  player.setPlaylist(nowPlayList);
+  player.setMusic();
   player.setPlayStatus(player.isPlaying());
-};
+});
 
-
-$prevBtn.onclick = () => {
+$prevBtn.addEventListener('click', () => {
   if (nowPlayList.length === 0) return;
 
-  player.playPrev(nowPlayList);
-};
+  player.setPlaylist(nowPlayList);
+  player.playPrev();
+});
 
-$nextBtn.onclick = () => {
+$nextBtn.addEventListener('click', () => {
   if (nowPlayList.length === 0) return;
 
+  player.setPlaylist(nowPlayList);
   player.playNext(nowPlayList);
-};
+});
+
+$playList.addEventListener('click', (e) => {
+  if (e.target.matches('.list-remove, .list-down, .list-up')) return;
+  const id = e.target.matches('li') ? +e.target.id.replace('pl-', '') : +e.target.parentNode.id.replace('pl-', '');
+
+  player.playSelectedList(id);
+});
+
+// shffle
+$shuffleBtn.addEventListener('click', player.setShuffleStatus);
+
 
 $musicPlayer.addEventListener('timeupdate', player.setProgToRuntime);
+
+$progOuter.addEventListener('click', player.setRuntimeToProg);
+
+$progGetevent.addEventListener('mousedown', (e) => {
+  document.addEventListener('mousemove', player.setRuntimeToProg);
+});
+
+document.addEventListener('mouseup', () => {
+  document.removeEventListener('mousemove', player.setRuntimeToProg);
+});
