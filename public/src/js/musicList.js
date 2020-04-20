5300b43b3ed48891c3ab30c2ad7f77f2451c3978
hodeng28 <ghepd28@gmail.com>
2020-04-20 21:23:18 +0900

    add: favorite btn

--- public/src/js/musicList.js
+++ public/src/js/musicList.js
@@ -1,14 +1,11 @@
 const $musicList = document.querySelector('.music-list');
 let $albumBlurs = document.querySelectorAll('.album-blur');
 let $albumBtns = document.querySelectorAll('.album-btn-set');
-const $music = document.querySelector('.music');
+let $favoriteBtns = document.querySelectorAll('.album-btn.favorite');
 
 // 재생목록 click event dom
 const $listOpenBtn = document.querySelector('.play-list-open');
 const $playListAll = document.querySelector('.play-list-all');
-
-
-// music top 10   dom
 
 
 $musicList.addEventListener('mouseover', ({ target }) => {
@@ -23,6 +20,7 @@ $musicList.addEventListener('mouseover', ({ target }) => {
   $albumBtns[index].classList.remove('hidden');
 });
 
+
 $musicList.addEventListener('mouseout', ({ target }) => {
   if (!target.matches('.album-img')) return;
 
@@ -36,21 +34,14 @@ $musicList.addEventListener('mouseout', ({ target }) => {
 });
 
 
-// if (!target.matches('.album-img', '.album-blur', '.album-btn-set') || target.parentNode.id === dsa) {
-//   $albumBtn.classList.add('hidden');
-//   $albumBlur.classList.add('hidden');
-// } else {
-//   $albumBtn.classList.remove('hidden');
-//   $albumBlur.classList.remove('hidden');
-// }
-
-
-// 재생목록 click event dom
+// 재생목록 click event
 $listOpenBtn.addEventListener('click', () => {
   $playListAll.classList.toggle('active');
 });
 
 
-// // music top 10
+// 즐겨찾기!
 
-// $ganre.addEventListener('click')
+$favoriteBtns.addEventListener('click', () => {
+  console.dir($favoriteBtns);
+});
