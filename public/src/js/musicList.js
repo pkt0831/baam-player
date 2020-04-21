6c0e1a5f55b5cb386ada1267ac9e022d286bfc7f
angryboo <buyongsong@gmail.com>
2020-04-21 15:52:57 +0900

    fix: fliker music list

--- public/src/js/musicList.js
+++ public/src/js/musicList.js
@@ -18,21 +18,21 @@ $musicList.addEventListener('mouseover', ({ target }) => {
 
   $albumBlurs[index].classList.remove('hidden');
   $albumBtns[index].classList.remove('hidden');
+
+  $albumBtns.forEach(btnset => {
+    btnset.addEventListener('mouseleave', ({ target }) => {
+    
+      $albumBlurs = document.querySelectorAll('.album-blur');
+      $albumBtns = document.querySelectorAll('.album-btn-set');
+    
+      const index = +target.parentNode.id.replace('ml-', '');
+      $albumBlurs[index].classList.add('hidden');
+      $albumBtns[index].classList.add('hidden');
+    });
+  })
 });
 
 
-$musicList.addEventListener('mouseout', ({ target }) => {
-
-  if (!target.matches('.album-btn-set')) return;
-
-  $albumBlurs = document.querySelectorAll('.album-blur');
-  $albumBtns = document.querySelectorAll('.album-btn-set');
-
-  const index = +target.parentNode.id.replace('ml-', '');
-
-  $albumBlurs[index].classList.add('hidden');
-  $albumBtns[index].classList.add('hidden');
-});
 
 
 // 재생목록 click event
