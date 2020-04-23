d7d13b6d9ca4b23b0994a4bb0fbdb918361438b1
Seongkyun-Yu <ysungkyun@gmail.com>
2020-04-23 18:13:57 +0900

    fix: fix bugs

--- public/src/js/signin.js
+++ public/src/js/signin.js
@@ -1,6 +1,8 @@
 import * as player from "./player.js";
 import * as interlock from './login.js';
 
+const PLAY_ON = false;
+const PLAY_OFF = true;
 
 const $signinIdInput = document.querySelector('.signin-id-input');
 const $signinPasswordInput = document.querySelector('.signin-password-input');
@@ -28,6 +30,9 @@ const $userInnerImgs = document.querySelectorAll('.user-inner-img');
 
 const $signinPopUp = document.querySelector('.signin-popup');
 const $userinfoSignoutBtn = document.querySelector('.userinfo-signout-btn');
+
+const $musicPlayer = document.querySelector('.musicPlayer');
+const $playBtn = document.querySelector('.player-play');
 
 
 // localstorage
@@ -111,12 +116,18 @@ const login = async (id, password) => {
       myStorage.setItem('email', data.email);
       myStorage.setItem('isuser', true);
 
+      $playBtn.childNodes[0].classList.remove('fa-pause');
+      $playBtn.childNodes[0].classList.add('fa-play');
+      $musicPlayer.pause();
+      $musicPlayer.currentTime = 0;
+
       await player.setPlayList.fromServer(id);
       player.setMusic();
       player.listRender();
       popSignCompleteWindow();
       renderUserInfo();
       setUserImage(data.id);
+      // player.setPlayStatus(PLAY_OFF);
     } else {
       // popup 추가할것
       popSignRejectText();
@@ -138,11 +149,17 @@ const logout = () => {
   myStorage.setItem('isuser', false);
   myStorage.setItem('email', 'call@gmail.com');
 
+  $playBtn.childNodes[0].classList.remove('fa-pause');
+  $playBtn.childNodes[0].classList.add('fa-play');
+  $musicPlayer.pause();
+  $musicPlayer.currentTime = 0;
+
   player.setMusic();
   player.clearPlayList();
 
   renderUserInfo();
   setUserImage('guest');
+  // player.setPlayStatus(PLAY_OFF);
 };
 
 
