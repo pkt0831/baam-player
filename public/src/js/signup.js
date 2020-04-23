98a609fdc36f9a7b0be533892e1ab583f1e0f331
angryboo <buyongsong@gmail.com>
2020-04-23 21:29:06 +0900

    fix: signup popup finction

--- public/src/js/signup.js
+++ public/src/js/signup.js
@@ -3,15 +3,21 @@ const $signupIdInput = document.querySelector('.signup-id-input');
 const $signupPasswordInput = document.querySelector('.signup-password-input');
 const $signupNameInput = document.querySelector('.signup-name-input');
 const $signupEmailInput = document.querySelector('.signup-email-input');
+const $signupCompletePopup = document.querySelector('.signup-complete-popup');
 const $rejectSignupMsg = document.querySelector('.reject-signup-msg');
+const $completeSignupMsg = document.querySelector('.signup-complete-msg');
+const $afterSigninBtn = document.querySelector('.signup-after-signin-btn');
+const $afterCancelBtn = document.querySelector('.signout-cancel-btn');
 
 
 const completeSignUp = message => {
   console.log(message);
+  $signupCompletePopup.classList.remove('hidden');
+  $completeSignupMsg.textContent = message;
 };
 
 const rejectSignUp = message => {
-  console.log(message);
+  $rejectSignupMsg.textContent = message;
 };
 
 const signup = async (id, name, password, email) => {
