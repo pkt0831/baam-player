94cd82bd3cb00a10071169ddd20eb5a8ac12f89f
hodeng28 <ghepd28@gmail.com>
2020-04-24 11:42:50 +0900

    fix: css

--- public/src/js/signin.js
+++ public/src/js/signin.js
@@ -1,6 +1,9 @@
 import * as player from "./player.js";
 import * as interlock from './login.js';
 
+const PLAY_ON = false;
+const PLAY_OFF = true;
+
 const $signinIdInput = document.querySelector('.signin-id-input');
 const $signinPasswordInput = document.querySelector('.signin-password-input');
 const $signinSigninBtn = document.querySelector('.signin-signin-btn');
@@ -33,17 +36,14 @@ const $playBtn = document.querySelector('.player-play');
 
 const $favoriteListAll = document.querySelector('.favorite-list-all');
 
-const $nomalIcon = document.querySelector('.normal-icon');
-const $premiumIcon = document.querySelector('.premium-icon');
-
 
 // localstorage
 const myStorage = window.localStorage;
 
 
-const setUserImage = () => {
+const setUserImage = fileName => {
   [...$userInnerImgs].forEach(img => {
-    img.style = `background-image: url(./css/user-img/${myStorage.id}.png)`;
+    img.style = `background-image: url(./css/user-img/${fileName}.png)`;
   });
   // $userInnerImg.style = `background-image: url(./css/user-img/${fileName}.png)`;
 };
@@ -94,14 +94,6 @@ const setUserInfo = (id, name, premium, email) => {
   myStorage.setItem('name', name);
   myStorage.setItem('premium', premium);
   myStorage.setItem('email', email);
-
-  if (premium) {
-    $nomalIcon.classList.add('hidden');
-    $premiumIcon.classList.remove('hidden');
-  } else {
-    $nomalIcon.classList.remove('hidden');
-    $premiumIcon.classList.add('hidden');
-  }
 };
 
 const renderUserInfo = () => {
@@ -110,9 +102,7 @@ const renderUserInfo = () => {
   $userName.textContent = myStorage.getItem('name');
   $userGrade.textContent = myStorage.getItem('premium') === 'true' ? '프리미엄 회원' : '일반회원';
   $userEmail.textContent = myStorage.getItem('email');
-  setUserInfo(myStorage.getItem('id'), myStorage.getItem('name'), myStorage.getItem('premium'), myStorage.getItem('email'));
   exchangeUserWindow();
-  setUserImage();
 };
 
 
@@ -138,7 +128,7 @@ const login = async (id, password) => {
       player.listRender();
       popSignCompleteWindow();
       renderUserInfo();
-      // setUserImage(data.id);
+      setUserImage(data.id);
       // player.setPlayStatus(PLAY_OFF);
     } else {
       // popup 추가할것
